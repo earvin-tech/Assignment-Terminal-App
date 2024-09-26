21e0d1431a3ed84ca39487354bf611aa66b50b1a
earvin-tech <wsl@Earvin>
2024-09-26 13:25:44 +1000

    Remove pycache

---
+++ functions/library_functions.py
@@ -0,0 +1,9 @@
+from classes.bookshelf import Bookshelf
+from classes.non_fiction_book import NonFictionBook
+from classes.fiction_book import FictionBook
+
+def add_book(library):
+    # Take title of book as input
+    # Take author as input
+    # Take
+    pass
