900dedecc081b2261b300f4ee6d70648997ebe97
earvin-tech <wsl@Earvin>
2025-04-15 01:26:58 +1000

    fix: save and exit function
    Made one json object per bookshelf, inside store books array, each book is a dictionary

--- functions/file_functions.py
+++ functions/file_functions.py
@@ -3,54 +3,80 @@ import json
 from classes.bookshelf import Bookshelf
 from classes.fiction_book import FictionBook
 
-def save_and_exit(library):
-    json_to_write1 = []
-    for bookshelf in library.get_all_bookshelves():
-            if bookshelf.get_books():
-                i = 0
-                for book in bookshelf.get_book_list():
-                    bookshelf_json = {
-                        "bookshelf_name": bookshelf.get_name(),
-                        f"book{i}": {
-                            f"title{i}": book.get_title(),
-                            f"author{i}": book.get_author(),
-                            f"rating{i}": book.get_rating(),
-                            f"genre{i}": book.get_genre()
-                        }
-                }
+# def save_and_exit(library):
+#     json_to_write1 = []
+#     for bookshelf in library.get_all_bookshelves():
+#             if bookshelf.get_books():
+#                 i = 0
+#                 for book in bookshelf.get_book_list():
+#                     bookshelf_json = {
+#                         "bookshelf_name": bookshelf.get_name(),
+#                         f"book{i}": {
+#                             f"title{i}": book.get_title(),
+#                             f"author{i}": book.get_author(),
+#                             f"rating{i}": book.get_rating(),
+#                             f"genre{i}": book.get_genre()
+#                         }
+#                 }
                 
-            else: 
-                bookshelf_json = {
-                    "bookshelf_name": bookshelf.get_name()
-                }
+#             else: 
+#                 bookshelf_json = {
+#                     "bookshelf_name": bookshelf.get_name()
+#                 }
 
-            json_to_write1.append(bookshelf_json)
+#             json_to_write1.append(bookshelf_json)
     
-    with open("data/library.json", "w") as json_file1:
-        json.dump(json_to_write1, json_file1, indent=4)
+#     with open("data/library.json", "w") as json_file1:
+#         json.dump(json_to_write1, json_file1, indent=4)
 
-    json_to_write2 = []
-    for book in library.get_all_books():
-        if book.get_genre():
-            books_json = {
-            "title_book": book.get_title(),
-            "author_book": book.get_author(),
-            "rating_book": book.get_rating(),
-            "genre": book.get_genre()
-        }
-        elif book.get_research_topic():
-            books_json = {
-            "title_book": book.get_title(),
-            "author_book": book.get_author(),
-            "rating_book": book.get_rating(),
-            "research_topic": book.get_research_topic()
-        }
-        json_to_write2.append(books_json)
+#     json_to_write2 = []
+#     for book in library.get_all_books():
+#         if book.get_genre():
+#             books_json = {
+#             "title_book": book.get_title(),
+#             "author_book": book.get_author(),
+#             "rating_book": book.get_rating(),
+#             "genre": book.get_genre()
+#         }
+#         elif book.get_research_topic():
+#             books_json = {
+#             "title_book": book.get_title(),
+#             "author_book": book.get_author(),
+#             "rating_book": book.get_rating(),
+#             "research_topic": book.get_research_topic()
+#         }
+#         json_to_write2.append(books_json)
 
-    with open("data/books.json", "w") as json_file2:
-        json.dump(json_to_write2, json_file2, indent=4)
+#     with open("data/books.json", "w") as json_file2:
+#         json.dump(json_to_write2, json_file2, indent=4)
         
+
+def save_and_exit(library):
+    bookshelf_data = []
+
+    for bookshelf in library.get_all_bookshelves():
+        books = []
+        for book in bookshelf.get_book_list():
+            books.append({
+                "title": book.get_title(),
+                "author": book.get_author(),
+                "rating": book.get_rating(),
+                "genre": getattr(book, "genre", None),
+                "research_topic": getattr(book, "research_topic", None)
+            })
         
+        bookshelf_data.append({
+            "bookshelf_name": bookshelf.get_name(),
+            "books": books
+        })
+    
+    with open("data/library.json", "w") as f:
+        json.dump(bookshelf_data, f, indent=4)
+
+
+
+
+
 
 def load_from_file(library):
     try:
